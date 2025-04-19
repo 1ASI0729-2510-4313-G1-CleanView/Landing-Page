163b6fae485b28447a691b98d72984d71d10b582
Mevi1217 <melgarejogomezmarcia@gmail.com>
2025-04-18 19:03:34 -0500

    Feat: faqs section and fix nav-var

--- js/script.js
+++ js/script.js
@@ -46,6 +46,33 @@ function toggleAnswer(faq) {
   answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
 }
 
+
+/////////////////////////////////////
+//FAQs
+
+(function () {
+  const titleQuestions = [...document.querySelectorAll(".name-Question")];
+  console.log(titleQuestions);
+
+  titleQuestions.forEach((question) => {
+      question.addEventListener("click", () => {
+      let height = 0;
+      let answer = question.nextElementSibling;
+      let addPadding = question.parentElement.parentElement;
+
+      addPadding.classList.toggle("faq-item--add");
+      question.children[0].classList.toggle("questions-arrow--rotate");
+
+      if (answer.clientHeight === 0) {
+          height = answer.scrollHeight;
+      }
+      answer.style.height = `${height}px`;
+      });
+  });
+})();
+
+
+
 /////////////////////////////
 
 
